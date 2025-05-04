87d6635617f80cee126a1384eb0ea1b361b74ec7
jesmine <khaijingtey0820@gmail.com>
2025-05-04 16:10:45 +0800

    Final Update TTS.py

--- pdf_chatbot/textToSpeech.py
+++ pdf_chatbot/textToSpeech.py
@@ -2,14 +2,16 @@ import pygame
 import speech_recognition as sr
 import os
 from gtts import gTTS
+import re
 
-class TTS():
-    # Initialize pygame mixer
+class TTS:
+    # Initialize the audio
     pygame.mixer.init()
 
+    @staticmethod
     def text_to_speech(text):
         tts = gTTS(text=text, lang='en')
-        filename = "pdf_chatbot\\temp.mp3"
+        filename = "temp.mp3"
         tts.save(filename)
 
         pygame.mixer.music.load(filename)
@@ -20,9 +22,9 @@ class TTS():
 
         os.remove(filename)
 
+    @staticmethod
     def speech_to_text():
         recognizer = sr.Recognizer()
-
         with sr.Microphone() as source:
             print("Say something...")
             recognizer.adjust_for_ambient_noise(source)
@@ -30,10 +32,32 @@ class TTS():
 
         try:
             text = recognizer.recognize_google(audio)
-            return text
+            return TTS.format_sentence(text)
         except sr.UnknownValueError:
-            print("Sorry, I could not understant what you said.")
+            print("Sorry, I could not understand what you said.")
             return None
-        except sr.RequestError as e:
+        except sr.RequestError:
             print("Could not request results from Google Speech Recognition.")
             return None
+
+    @staticmethod
+    def format_sentence(text):
+        text = text.strip()
+        if not text:
+            return ""
+
+        text = text[0].upper() + text[1:]
+
+        if not re.search(r'[.?!]$', text):
+            if TTS.is_question(text):
+                text += '?'
+            else:
+                text += '.'
+
+        return text
+
+    @staticmethod
+    def is_question(text):
+        question_words = ("who", "what", "when", "where", "why", "how", "can", "do", "does", "is", "are", "will", "did", "should", "could")
+        return text.lower().split()[0] in question_words
+
